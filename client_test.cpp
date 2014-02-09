23947adeb236be927f991074ebd397f976e69a2f
Jailfindery <joshua.g.schell@gmail.com>
2014-02-09 00:21:53 -0600

    Added exception handling to test files

--- client_test.cpp
+++ client_test.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 
-#include "server_connection.h"
 #include "inet_port.h"
+#include "jailket_except.h"
+#include "server_connection.h"
 
 using namespace std;
 using namespace jailket;
 
 int main()
 {
-    inet_port p("tcp", 8080);
-    server_connection s("127.0.0.1", p);
+    try
+    {
+        inet_port p("tcp", 8080);
+        server_connection s("127.0.0.1", p);
 
-    s.send("Hello, world!");
-    string reply = s.recv();
-    cout << reply << endl;
+        s.send("Hello, world!");
+        string reply = s.recv();
+        cout << reply << endl;
 
-    s.disconnect();
+        s.disconnect();
+    }
+    catch(jailket_invalid_argument& e)
+    {
+        cerr << e.what() << endl;
+        return 1;
+    }
+    catch(jailket_error& e)
+    {
+        cerr << e.what() << endl;
+        return 1;
+    }
 }
 
--- server_test.cpp
+++ server_test.cpp
@@ -2,6 +2,7 @@
 
 #include "client_connection.h"
 #include "inet_port.h"
+#include "jailket_except.h"
 #include "server.h"
 
 using namespace std;
@@ -9,15 +10,28 @@ using namespace jailket;
 
 int main()
 {
-    inet_port p("tcp", 8080);
-    server s(p);
+    try
+    {
+        inet_port p("tcp", 8080);
+        server s(p);
 
-    s.listen();
-    client_connection c = s.accept();
-    string mesg = c.recv();
-    c.send("ACK");
-    c.disconnect();
+        s.listen();
+        client_connection c = s.accept();
+        string mesg = c.recv();
+        c.send("ACK");
+        c.disconnect();
 
-    cout << mesg << endl;
+        cout << mesg << endl;
+    }
+    catch(jailket_invalid_argument& e)
+    {
+        cerr << e.what() << endl;
+        return 1;
+    }
+    catch(jailket_error& e)
+    {
+        cerr << e.what() << endl;
+        return 1;
+    }
 }
 
